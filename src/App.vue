a7b40df56c5567b4efcfd60800987ea16d6072ae
Victoria А <barkovavika89@gmail.com>
2024-10-08 21:31:38 +0300

    zero-point of project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header></header>
+
+  <main class="my-0 mx-auto box-border flex items-center justify-center">
+    <p class="textMain text-lg text-purple-600">Oh gosh poor little meow meows</p>
+  </main>
+
+  <footer></footer>
+</template>
+
+<style scoped></style>
