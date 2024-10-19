b56166bf128d1902205f813fc80489109913aab3
Victoria А <barkovavika89@gmail.com>
2024-10-19 23:28:35 +0300

    modified:   src/components/resources/MainResources.vue

--- src/components/resources/MainResources.vue
+++ src/components/resources/MainResources.vue
@@ -47,19 +47,19 @@
 
     <!-- 2nd block -->
     <div
-      class="col-start-5 col-end-9 row-start-7 row-end-8 bg-customPurple flex items-center justify-center p-5 rounded-[100px]"
+      class="col-start-3 col-end-6 row-start-7 row-end-8 bg-customPurple flex items-center justify-center p-5 rounded-[100px]"
     >
-      <p class="text-white font-myUnbounded font-medium text-[22px]">Adobe Illusrator</p>
+      <p class="text-white font-myUnbounded font-medium text-[22px]">Pinterest</p>
     </div>
     <div
-      class="col-start-1 col-end-4 row-start-8 row-end-9 bg-customLime p-5 rounded-[100px] flex items-center justify-center"
+      class="col-start-8 col-end-11 row-start-7 row-end-8 bg-customLime p-5 rounded-[100px] flex items-center justify-center"
     >
-      <p class="text-customBlack font-myUnbounded font-medium text-[22px]">Figma</p>
+      <p class="text-customBlack font-myUnbounded font-medium text-[22px]">FreePik</p>
     </div>
     <div
-      class="col-start-10 col-end-13 row-start-8 row-end-9 bg-customLime p-5 rounded-[100px] flex items-center justify-center"
+      class="col-start-1 col-end-4 row-start-9 row-end-10 bg-customLime p-5 rounded-[100px] flex items-center justify-center"
     >
-      <p class="text-customBlack font-myUnbounded font-medium text-[22px]">CorelDraw</p>
+      <p class="text-customBlack font-myUnbounded font-medium text-[22px]">Behance</p>
     </div>
     <div
       class="col-start-5 col-end-9 row-start-9 row-end-10 border-8 border-customOrange p-5 rounded-[100px] flex items-center justify-center"
@@ -67,19 +67,19 @@
       <p class="text-white font-myUnbounded font-medium text-[22px]">полезные сайты</p>
     </div>
     <div
-      class="col-start-1 col-end-4 row-start-10 row-end-11 bg-customPurple p-5 rounded-[100px] flex items-center justify-center"
+      class="col-start-10 col-end-13 row-start-9 row-end-10 bg-customPurple p-5 rounded-[100px] flex items-center justify-center"
     >
-      <p class="text-white font-myUnbounded font-medium text-[22px]">InDesign</p>
+      <p class="text-white font-myUnbounded font-medium text-[22px]">Dribble</p>
     </div>
     <div
-      class="col-start-10 col-end-13 row-start-10 row-end-11 bg-customPurple p-5 rounded-[100px] flex items-center justify-center"
+      class="col-start-3 col-end-6 row-start-11 row-end-12 bg-customPurple p-5 rounded-[100px] flex items-center justify-center"
     >
-      <p class="text-white font-myUnbounded font-medium text-[22px]">ProCreate</p>
+      <p class="text-white font-myUnbounded font-medium text-[22px]">Bookcoverarchive</p>
     </div>
     <div
-      class="col-start-5 col-end-9 row-start-11 row-end-12 bg-customLime flex items-center justify-center p-5 rounded-[100px]"
+      class="col-start-8 col-end-11 row-start-11 row-end-12 bg-customLime flex items-center justify-center p-5 rounded-[100px]"
     >
-      <p class="text-customBlack font-myUnbounded font-medium text-[22px]">Adobe Photoshop</p>
+      <p class="text-customBlack font-myUnbounded font-medium text-[22px]">Awwwards</p>
     </div>
     <div
       class="col-start-1 col-end-13 row-start-12 row-end-13 h-[5px] bg-white self-center w-full"
